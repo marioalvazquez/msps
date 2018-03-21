8bd9696c3895319459a260d307fe60d19a73c79b
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2018-03-20 20:43:51 -0600

    removed white spaces after callign to header

--- register.php
+++ register.php
@@ -19,7 +19,6 @@ if ( $result->num_rows > 0 ) {
 
     $_SESSION['message'] = 'Ya existe un usuario con esta cuenta de correo electrónico';
     header("location:error.php");
-
 }
 else { // Email doesn't already exist in a database, proceed...
 
@@ -53,12 +52,10 @@ else { // Email doesn't already exist in a database, proceed...
         mail( $to, $subject, $message_body, $headers);
 
         header("location:profile.php");
-
     }
 
     else {
         $_SESSION['message'] = 'Algo salió mal, vuelve a intentar más tarde!';
         header("location:error.php");
     }
-
 }
