1bf58a5bba1e08b0dbe70c955d4631eba8cb7576
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2018-04-02 20:14:31 -0500

    Changed verify function

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 /* Verifies registered user email, the link to this page
    is included in the register.php email message
 */
-require 'db.php';
+require 'connect.php';
 session_start();
 
 // Make sure email and hash variables aren't empty
@@ -12,7 +12,7 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
     $hash = $mysqli->escape_string($_GET['hash']);
 
     // Select user with matching email and hash, who hasn't verified their account yet (active = 0)
-    $result = $mysqli->query("SELECT * FROM users WHERE email='$email' AND hash='$hash' AND active='0'");
+    $result = $mysqli->query("SELECT * FROM user WHERE email='$email' AND hash='$hash' AND active='0'");
 
     if ( $result->num_rows == 0 )
     {
