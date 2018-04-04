cbaa7ce5c19c2e2bc767b46934e70454ed731f9d
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2018-04-03 20:28:48 -0500

    Fix header

--- login2.php
+++ login2.php
@@ -23,7 +23,7 @@ else { // User exists
         // This is how we'll know the user is logged in
         $_SESSION['logged_in'] = true;
 
-        header("location: profile.php");
+        echo "<script>location.href = 'profile.php'</script>";
     }
     else {
         $_SESSION['message'] = "Contraseña incorrecta, intenta de nuevo!";
