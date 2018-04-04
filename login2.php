4f4041c54bc3e61aecd2ff20e06b22a2702637fa
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2018-04-03 20:02:18 -0500

    Intent to solve header not working on live

--- login2.php
+++ login2.php
@@ -1,13 +1,15 @@
 <?php
 /* User login process, checks if user exists and password is correct */
-
+ob_start();
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', 1);
 // Escape email to protect against SQL injections
 $email = $mysqli->escape_string($_POST['email']);
 $result = $mysqli->query("SELECT * FROM user WHERE email='$email'");
 
 if ( $result->num_rows == 0 ){ // User doesn't exist
     $_SESSION['message'] = "El usuario con el correo electrónico ingresado no existe";
-    header("location: http://msps.mariovazquez.com.mx/error.php");
+    header("location: error.php");
 }
 else { // User exists
     $user = $result->fetch_assoc();
@@ -25,6 +27,6 @@ else { // User exists
     }
     else {
         $_SESSION['message'] = "Contraseña incorrecta, intenta de nuevo!";
-        header("location: http://msps.mariovazquez.com.mx/error.php");
+        header("location: error.php");
     }
 }
